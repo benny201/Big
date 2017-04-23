5c7b1309536bf97c12e5bb6772a0364577e6ced6
447093943@qq.com <zwu58@ucsc.edu>
2017-04-23 00:58:40 -0700

    stock features done

--- routes/stock.js
+++ routes/stock.js
@@ -91,54 +91,145 @@ router.get('/', function(req, res, next) {
     .catch(next);
 });
 
+
+// //get data
+// var getArticles = function(type, articleId) {
+//   type
+//   .checkArticleById(articleId)
+//   .then(function(result) {
+//     res.render('single-stock', { stock: result })
+//   })
+// }
+
+
 // sign up page
 router.get('/:type/:articleId', function(req, res, next) {
   var type = req.params.type;
   var articleId = req.params.articleId;
+
+  getArticles(type, articleId);
 
   if (type == 'Gushenqu') {
     GushenquModel
       .checkArticleById(articleId)
       .then(function(result) {
         res.render('single-stock', { stock: result })
-      })
-  }
+      });
+  };
   if (type == 'Fandeyibiao') {
     FandeyibiaoModel
     .checkArticleById(articleId)
     .then(function(result) {
       res.render('single-stock', { stock: result })
-    })
-  }
+    });
+  };
   if (type == 'Gufengle') {
     GufengleModel
     .checkArticleById(articleId)
     .then(function(result) {
       res.render('single-stock', { stock: result })
-    })
-  }
+    });
+  };
   if (type == 'Caijingmeinv') {
     CaijingmeinvModel
     .checkArticleById(articleId)
     .then(function(result) {
       res.render('single-stock', { stock: result })
-    })
-  }
+    });
+  };
   if (type == 'Chaoduanxiaochunjie') {
     ChaoduanxiaochunjieModel
     .checkArticleById(articleId)
     .then(function(result) {
-      res.render('single-stock', { stock: result })
-    })
-  }
+      res.render('single-stock', { stock: result });
+    });
+  };
   if (type == 'Linqikanpan') {
     LinqikanpanModel
     .checkArticleById(articleId)
     .then(function(result) {
       res.render('single-stock', { stock: result })
-    })
-  }
+    });
+  };
 
 });
+
+router.get('/:blogName', function(req, res, next) {
+    var blogName = req.params.blogName;
+    var renderInput = {};
+
+    if (blogName == 'Gushenqu') {
+      GushenquModel
+        .getArticles()
+        .then(function(result) {
+          renderInput.blogContent = result;
+          renderInput.blogName = "股社区";
+          renderInput.picturename = blogName;
+          res.render('allstock', { renderInput: renderInput });
+        });
+    };
+
+    if (blogName == 'Fandeyibiao') {
+      FandeyibiaoModel
+      .getArticles()
+      .then(function(result) {
+        renderInput.blogContent = result;
+        renderInput.blogName = "范德依彪";
+        renderInput.picturename = blogName;
+        res.render('allstock', { renderInput: renderInput });
+      });
+    };
+
+    if (blogName == 'Gufengle') {
+      GufengleModel
+      .getArticles()
+      .then(function(result) {
+        renderInput.blogContent = result;
+        renderInput.blogName = "股蜂乐";
+        renderInput.picturename = blogName;
+        res.render('allstock', { renderInput: renderInput });
+      });
+    };
+
+    if (blogName == 'Caijingmeinv') {
+      CaijingmeinvModel
+      .getArticles()
+      .then(function(result) {
+        renderInput.blogContent = result;
+        renderInput.blogName = "财经美女蜗牛妹";
+        renderInput.picturename = blogName;
+        res.render('allstock', { renderInput: renderInput });
+      });
+    };
+
+
+    if (blogName == 'Chaoduanxiaochunjie') {
+      ChaoduanxiaochunjieModel
+      .getArticles()
+      .then(function(result) {
+        renderInput.blogContent = result;
+        renderInput.blogName = "超短小纯杰";
+        renderInput.picturename = blogName;
+        res.render('allstock', { renderInput: renderInput });
+      });
+    };
+
+
+    if (blogName == 'Linqikanpan') {
+      LinqikanpanModel
+      .getArticles()
+      .then(function(result) {
+        renderInput.blogContent = result;
+        renderInput.blogName = "林奇看盘";
+        renderInput.picturename = blogName;
+        res.render('allstock', { renderInput: renderInput });
+    });
+  };
+
+
+
+});
+
+
 
 module.exports = router;
